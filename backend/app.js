939fe8ecba5cb28b3a1392f9e8e76e8fbdeef38b
mustufakhan <mohdmustufa568@gmail.com>
2021-04-15 17:08:22 +0530

    backend changes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose  = require('mongoose')
+const {MONGOURI} = require('./config/keys')
+
+mongoose.connect(MONGOURI,{
+	useNewUrlParser:true,
+	useUnifiedTopology: true
+})
+mongoose.connection.on('connected',()=>{
+	console.log("conneted to mongo yeahh")
+})
+mongoose.connection.on('error',(err)=>{
+	console.log("err connecting",err)
+})
 
 app.listen(4000,()=>{
 	console.log('server running')
